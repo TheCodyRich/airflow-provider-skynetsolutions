85bea61a88e21426f2e6e9326fdac552059db9de
Cody Rich <83995939+TheCodyRich@users.noreply.github.com>
2023-04-05 18:19:19 -0400

    Defaulted to davinci 3

--- chatgpt_operator.py
+++ chatgpt_operator.py
@@ -5,7 +5,7 @@ import openai
 
 class ChatGPTOperator(BaseOperator):
     @apply_defaults
-    def __init__(self, api_key, prompt, *args, max_tokens=1024, temperature=0.5, engine="text-davinci-002", **kwargs):
+    def __init__(self, api_key, prompt, *args, max_tokens=1024, temperature=0.5, engine="text-davinci-003", **kwargs):
         super().__init__(*args, **kwargs)
         self.api_key = api_key
         self.prompt = prompt
