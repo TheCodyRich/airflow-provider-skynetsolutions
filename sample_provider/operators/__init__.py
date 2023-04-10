f4f7a7100c405f34a557b3bb5fc54b3dc06c268f
Cody Rich <83995939+TheCodyRich@users.noreply.github.com>
2023-04-10 10:35:38 -0400

    added tests

---
+++ sample_provider/operators/__init__.py
@@ -0,0 +1,7 @@
+def get_provider_info():
+    return {
+        "package-name": "airflow-provider-thecodyrich",
+        "name": "TheCodyRich Provider",
+        "description": "An Apache Airflow provider for all things made by TheCodyRich",
+        "versions": ["0.0.1"],
+    }
