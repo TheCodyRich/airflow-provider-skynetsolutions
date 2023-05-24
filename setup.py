e37b361190784dd89bd3855722dca03630976b48
Cody Rich <83995939+TheCodyRich@users.noreply.github.com>
2023-05-24 11:51:39 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ with open("README.md", "r") as fh:
 __version__ = "1.0.0"
 
 setuptools.setup(
-    name="airflow-provider-thecodyrich",
-    version=thecodyrich_provider.__version__,
+    name="airflow-provider-skynetsolutions",
+    version=skynetsolutions_provider.__version__,
     author="TheCodyRich",
     license="Apache License 2.0",
     description="An Apache Airflow provider for all things made by TheCodyRich",
@@ -15,9 +15,9 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     entry_points="""
         [apache_airflow_provider]
-        provider_info=thecodyrich_provider.__init__:get_provider_info
+        provider_info=skynetsolutions_provider.__init__:get_provider_info
     """,
-    url="https://github.com/TheCodyRich/airflow-provider-thecodyrich",
+    url="https://github.com/TheCodyRich/airflow-provider-skynetsolutions",
     install_requires=[
         "apache-airflow>=2.2",
         "openai>=0.27.4"
